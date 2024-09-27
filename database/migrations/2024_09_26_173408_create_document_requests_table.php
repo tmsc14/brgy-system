d836f3ccfaee36d9e2afb8b0bf85dbe169ea29bb
jemiamanaid <jemiam@pabacus.com>
2024-09-27 15:48:01 +0800

    Migration to add document_requests table

---
+++ database/migrations/2024_09_26_173408_create_document_requests_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('document_requests', function (Blueprint $table) {
+            $table->unsignedBigInteger('barangay_id');
+            $table->id();
+            $table->unsignedBigInteger('resident_id');
+            $table->string('document_owner_name');
+            $table->string('document_type');
+            $table->text('document_data_json');
+            $table->text('document_file_urls_csv');
+            $table->string('status');
+            $table->timestamps();
+
+            $table->foreign('barangay_id')->references('id')->on('barangays')->onDelete('cascade');
+            $table->foreign('resident_id')->references('id')->on('barangay_residents')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_requests');
+    }
+};
