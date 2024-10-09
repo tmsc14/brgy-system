512e9c1728dc1995e3da1842cdf103c72351508e
jemiamanaid <jemiam@pabacus.com>
2024-10-10 06:31:26 +0800

    Fix statistics ui a bit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AccessCodeSeeder::class,
             FeatureSeeder::class,
-            FeaturePermission::class,
+            FeaturePermissionSeeder::class,
         ]);
     }
 }
